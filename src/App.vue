122be06f433e8e34bb1409b728419136f051d790
jkallson <janar.kallson@gmail.com>
2019-10-31 14:46:40 +0200

    Added footer component

--- src/App.vue
+++ src/App.vue
@@ -80,25 +80,18 @@
                 </div>
             </section>
         </section>
-        <footer>
-            <ul class="links">
-                <li>
-                    <a href="https://ois2.ut.ee/" target="_blank">OIS</a>
-                </li>
-                <li>
-                    <a href="https://courses.cs.ut.ee/" target="_blank">Courses</a>
-                </li>
-            </ul>
-        </footer>
+        <Footer></Footer>
     </main>
 </template>
 
 <script>
     import Header from "./components/Header";
+    import Footer from "./components/Footer";
     export default {
         name: 'app',
         components: {
             Header,
+            Footer
         }
     }
 </script>
@@ -127,33 +120,7 @@
         clear: both;
     }
 
-    footer {
-        padding: 30px 0;
-        background-color: #607D8B;
-        margin-top: 10px;
-        height: 100px;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-    }
 
-    footer .links {
-        display: block;
-        width: 100%;
-        max-width: 200px;
-        margin: 0 auto;
-        color: #acd7ff;
-        font-size: 11px;
-    }
-
-    footer .links a {
-        text-decoration: none;
-        color: #acd7ff;
-    }
-
-    footer .links a:hover {
-        text-decoration: underline;
-    }
 
     #container {
         width: 80%;
