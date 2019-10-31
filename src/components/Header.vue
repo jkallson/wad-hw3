8c52cac83d59f0104788966aa6ebad48c15c229e
jkallson <janar.kallson@gmail.com>
2019-10-31 14:49:09 +0200

    Actually added these components to git

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <strong>Welcome to your dashboard!</strong>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style>
+    header {
+        padding: 20px;
+        background-color: #2196F3;
+        color: #ffffff;
+        text-align: center;
+        margin-bottom: 10px;
+        height: 60px;
+    }
+
+</style>
