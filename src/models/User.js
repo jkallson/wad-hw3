b08783ef1986618a9e46d1d15e87afa5822a0c00
jkallson <janar.kallson@gmail.com>
2019-10-31 14:32:30 +0200

    Created User and Course javascript classes

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+export default class User{
+    constructor(firstName,lastName,birthDate,faculty,gpa){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.faculty = faculty;
+        this.gpa = gpa;
+    }
+}
